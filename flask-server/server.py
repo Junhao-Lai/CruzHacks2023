0eb6d8e3ebd1c03dd5f05989ab28dc7716704c80
6033llx <lli158@ucsc.edu>
2023-02-04 14:19:55 -0800

    slight modification on server.py under flask-server

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,7 +1,39 @@
-from flask import Flask
+from flask import Flask, request, Response
+from flask_sqlalchemy import SQLAlchemy
+import json, requests
+
 app = Flask(__name__)
-@app.route('/')
-def members():
-	return {"members": ["Michael","Claude","Vincent","Junhao"]}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgresuper@localhost:4848/allergy_menus'
+db = SQLAlchemy(app)
+
+# @app.route('/')
+# def members():
+# 	return {"message" : "yo, this is your allergy reminder","members": ["Michael","Claude","Vincent","Junhao"]}
+
+@app.route('/', methods=['GET'])
+def response_allergy_dishes():
+	status_code = 500
+	request_json = request.get_json()
+
+	try:
+		allergen = request_json['allergen']
+		location = request_json['location']
+		date = request_json['date']
+
+		if len(date) != 3:
+			return Response(json.dumps({"Error": "Internal Error"}), status_code)
+
+
+		status_code = 200
+
+		return Response(json.dumps({
+			"Message": "This is a response"
+		}), status_code)
+	except (KeyError):
+		return Response(json.dumps({"Error": "Internal Error"}), status_code)
+
+
+if __name__ == "__main__":
+	app.run(debug=True, host='0.0.0.0', port = "4848")
 
 	
