b2ef9a883c9d279f9f59b1087e8bbd8162da6644
JUnhao <jlai38@ucsc.edu>
2023-02-04 14:07:46 -0800

    14:07 from 赖俊豪

--- tecmint-app/src/App.js
+++ tecmint-app/src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+//import axios from 'axios';
+//import {useState, useEffect} from 'react';
+//useeffect allows you to fetch component before its mounted.
 
-function App() {
+function MyButton() {
+  const options = ["Milk", "Egg", "Peanuts"]
   return (
-    <div className="App">
-      <h1>Welcome to Cruzhacks 2023</h1>
+    <select>
+      <option>Please select an allergen.</option>
+      {options.map((option,index)=>{
+        return <option key = {index}>
+          {option}
+        </option>
+
+      })}
+    </select>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>UCSC Allergen Reminder</h1>
+      <MyButton />
     </div>
   );
 }
-export default App;
+//export default App;
