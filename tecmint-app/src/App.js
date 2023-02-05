a56a4715bcbed2873db710d4a356ba5773000d73
VincentS1477 <124420210+VincentS1477@users.noreply.github.com>
2023-02-04 20:54:35 -0800

    Changes by Vincent Siu. Working UI.

--- tecmint-app/src/App.js
+++ tecmint-app/src/App.js
@@ -6,10 +6,11 @@ document.body.style.backgroundColor = 'black';
 document.body.style.color = 'white';
 
 function Food() {
-  const options = ["Milk", "Egg", "Peanuts", "Fish", "Shrimp"]
+  const options = ["Milk", "Egg", "Peanuts", "Fish", "Shellfish", "Fish", "Soy", "Tree Nut", "Sesame", ]
   return (
-    <select>
+    <select id = "AllergenSelect">
       <option>Please select an allergen.</option>
+
       {options.map((option,index)=>{
         return <option key = {index}>
           {option}
@@ -21,9 +22,9 @@ function Food() {
 }
 
 function Hall() {
-  const options = ["9/10 Dining Hall", "Porter", "Crown", "Cowell"]
+  const options = ["College 9/ Lewis Dining Hall", "Porter", "Crown", "Cowell"]
   return (
-    <select>
+    <select id = "HallSelect">
       <option>Please select a dining hall</option>
       {options.map((option,index)=>{
         return <option key = {index}>
@@ -34,12 +35,12 @@ function Hall() {
   );
 }
 
-function Day() {
-  const options = ["Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday","Sunday"]
+function DayOfWeek(){
+  const day = ["Sunday", "Monday", "Tuesday", "Wesdneday.", "Thurday", "Friday", "Saturday"]
   return (
-    <select>
-      <option>Please select the Day</option>
-      {options.map((option,index)=>{
+    <select id = "DaySelect">
+      <option>Please select the day of the week you are going</option>
+      {day.map((option,index)=>{
         return <option key = {index}>
           {option}
         </option>
@@ -51,15 +52,42 @@ function Day() {
 
 
 
+
 export default function MyApp() {
+
+  var width = (window.innerWidth/(2)-170);
+  var text = document.getElementById('test')
+  text.style.position = 'absolute'
+  text.style.top = '20px'
+  text.style.left = `${width}px`;
+
+  var width2 = (window.innerWidth/8);
+  var text1 = document.getElementById('AllergenSelect')
+  text1.style.position = 'absolute'
+  text1.style.top = '150px'
+  text1.style.left = `${width2}px`;
+
+  var width3 = (window.innerWidth/2)-70;
+  var text2 = document.getElementById('HallSelect')
+  text2.style.position = 'absolute'
+  text2.style.top = '150px'
+  text2.style.left = `${width3}px`;
+
+  var width4 = (window.innerWidth)-430;
+  var text3 = document.getElementById('DaySelect')
+  text3.style.position = 'absolute'
+  text3.style.top = '150px'
+  text3.style.left = `${width4}px`;
+
   return (
+    
     <div>
-      <h1>UCSC Allergen Reminder</h1>
+      <h1 id = "test">UCSC Allergen Reminder</h1>
       <img src="https://nutrition.sa.ucsc.edu/images/dining-logo-foodpro.png" alt="Example Image"/>
+      "comment: DO NOT HIT REFRESH."
+      <DayOfWeek/>
       <Food />
       <Hall />
-      <Day />      
     </div>
   );
 }
-//export default App;
