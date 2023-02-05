80933a8a9dfd065d41df3c5940c6f85eb8965d98
6033llx <lli158@ucsc.edu>
2023-02-05 02:52:49 -0800

    connect the database and backend!

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,47 +1,57 @@
 from flask import Flask, request, Response
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import PrimaryKeyConstraint
+from sqlalchemy import PrimaryKeyConstraint, and_
 import json, requests
+import psycopg2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgresuper@localhost:3000/allergy_menus'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgresuper@localhost:5432/allergy_reminder'
 db = SQLAlchemy(app)
 
+conn = psycopg2.connect(
+	host="localhost",
+	port = "5432",
+	database = "allergy_reminder",
+	user = "postgres",
+	password="postgresuper"
+)
 
-class Allergy(db.Model):
-	dish_name = db.Column(db.String(100), nullable=False, primary_key=True)
-	allergen = db.Column(db.String(40), nullable=False, primary_key=True)	
-	__table_args__ = (PrimaryKeyConstraint('dish_name', 'allergen', name='pk_dishName_allergen'),)
+conn.set_isolation_level(0)
+cursor = conn.cursor()
+cursor.execute("SET SCHEMA 'allergy_menu'")
+conn.commit()
 
-	def __repr__(self) -> str:
-		return f"Allergy: {self.description}"
-	
-	def __init__(self, description) -> None:
-		self.description = description
-
+class Allergens(db.Model):
+	__tablename__ = 'allergens'
+	dishname = db.Column(db.String(100), nullable=False, primary_key=True)
+	allergenname = db.Column(db.String(40), nullable=False, primary_key=True)	
+	__table_args__ = (PrimaryKeyConstraint('dishname', 'allergenname', name='pk_dishName_allergen'),)
 
 
 class Menus(db.Model):
-	dish_name = db.Column(db.String(100), nullable=False, primary_key=True)
-	location = db.Column(db.String(40), nullable=False, primary_key=True)
-	date = db.Column(db.String(3), nullable=False, primary_key=True)
-	__table_args__ = (PrimaryKeyConstraint('dish_name', 'location', 'date', name='pk_dishName_location_date'),)
+	__tablename__ = 'menus'
+	dishname = db.Column(db.String(100), nullable=False, primary_key=True)
+	dishlocation = db.Column(db.String(40), nullable=False, primary_key=True)
+	dishdate = db.Column(db.String(3), nullable=False, primary_key=True)
+	__table_args__ = (PrimaryKeyConstraint('dishname', 'dishlocation', 'dishdate', name='pk_dishName_location_date'),)
 
 
 # this function is used to get the list of allergy dishes
 def get_dish_list(location, date, allergen):
 	dish_list = []
-	print("Good before get all in menu_dishes")
-	menu_dishes = db.session.query(Menus).filter(Menus.location == location, Menus.date == date).all()
-	print("Good after get all in menu_dishes")
-	print(menu_dishes)
-	print("Good before for")
+	#print("Connecting to the database")
+	menu_dishes = db.session.query(Menus).filter(Menus.dishlocation == location, Menus.dishdate == date).all()
+	if(menu_dishes == None):
+		return dish_list
+	# print("Successfully connected to the database")
+	# print(menu_dishes)
+	# print("Good before for")
 	for menu_dish in menu_dishes:
-		allergy_dish = db.session.query(Allergy).filter(Allergy.dishName == menu_dish.dishName, Allergy.allergen == allergen).first()
-		print("Good after get first from allergy_dish")
+		allergy_dish =Allergens.query.filter(Allergens.dishname == menu_dish.dishname, Allergens.allergenname == allergen).first()
+		# fix the name things
 		if allergy_dish:
 			dish_list.append({
-				"name": allergy_dish.dishName
+				"name" : allergy_dish.dishname
 			})
 
 	return dish_list
@@ -55,7 +65,7 @@ def get_dish_list(location, date, allergen):
 def response_allergy_dishes():
 	status_code = 500
 	request_json = request.get_json()
-
+	print(request_json)
 	try:
 		allergen = request_json['allergen']
 		location = request_json['location']
@@ -69,7 +79,7 @@ def response_allergy_dishes():
 		status_code = 200
 		if len(dish_list) == 0:
 			return Response(json.dumps({
-			"Message": "Seems you would be safe there cuz there is not allergy dishes found!"
+			"Message": "Seems you would be safe there cuz there is no allergy dishes found!"
 			}), status_code)
 		
 
@@ -83,6 +93,6 @@ def response_allergy_dishes():
 
 
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0', port = "3000")
+	app.run(debug=True, port = "5432")
 
 	
