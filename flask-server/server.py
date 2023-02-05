100b7dc38d83499cf4d30ff2279e27aa2c5d74ea
6033llx <lli158@ucsc.edu>
2023-02-04 18:31:23 -0800

    meet stage goal in backend

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,51 @@
 from flask import Flask, request, Response
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import PrimaryKeyConstraint
 import json, requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgresuper@localhost:4848/allergy_menus'
 db = SQLAlchemy(app)
+
+
+class Allergy(db.Model):
+	dish_name = db.Column(db.String(100), nullable=False, primary_key=True)
+	allergen = db.Column(db.String(40), nullable=False, primary_key=True)	
+	__table_args__ = (PrimaryKeyConstraint('dish_name', 'allergen', name='pk_dishName_allergen'),)
+
+	def __repr__(self) -> str:
+		return f"Allergy: {self.description}"
+	
+	def __init__(self, description) -> None:
+		self.description = description
+
+
+
+class Menu(db.Model):
+	dish_name = db.Column(db.String(100), nullable=False, primary_key=True)
+	location = db.Column(db.String(40), nullable=False, primary_key=True)
+	date = db.Column(db.String(3), nullable=False, primary_key=True)
+	__table_args__ = (PrimaryKeyConstraint('dish_name', 'location', 'date', name='pk_dishName_location_date'),)
+
+	def __repr__(self) -> str:
+		return f"Menu: {self.description}"
+
+	def __init__(self, description) -> None:
+		self.description = description
+
+# this function is used to get the list of allergy dishes
+def get_dish_list(location, date, allergen):
+	dish_list = []
+	menu_dishes = db.session.query(Menu).filter(Menu.location == location, Menu.date == date).all()
+	for menu_dish in menu_dishes:
+		allergy_dish = db.session.query(Allergy).filter(Allergy.dishName == menu_dish.dishName, Allergy.allergen == allergen).first()
+		if allergy_dish:
+			dish_list.append({
+				"name": allergy_dish.dishName
+			})
+
+	return dish_list
+			
 
 # @app.route('/')
 # def members():
@@ -24,12 +65,20 @@ def response_allergy_dishes():
 			return Response(json.dumps({"Error": "Internal Error"}), status_code)
 
 
+		dish_list = get_dish_list()
 		status_code = 200
+		if len(dish_list) == 0:
+			return Response(json.dumps({
+			"Message": "Seems you would be safe there cuz there is not allergy dishes found!"
+			}), status_code)
+		
 
 		return Response(json.dumps({
-			"Message": "This is a response"
+			"Message": "Here are the allergy dishes you need to avoid!",
+			"Dish_list": dish_list
 		}), status_code)
-	except (KeyError):
+
+	except:
 		return Response(json.dumps({"Error": "Internal Error"}), status_code)
 
 
